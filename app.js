a398f3a1fed2f60b5125adf57b3e51663e975cb6
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-16 20:21:50 +0200

    update score and change active player

--- app.js
+++ app.js
@@ -32,6 +32,23 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
   diceImage.src = "dice-" + dice + ".png";
 
   // 3. Update the round score if the rolled number was NOT a 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
 });
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
